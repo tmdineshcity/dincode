9be0b70357a5bfdc64c6e496ba15d051b5342ebf
dinesh <dineshmsd40@gmail.com>
2025-03-08 17:22:34 +0530

    Initial Commit

---
+++ src/main/java/com/dinfactory/dincode/Controller/DincodeController.java
@@ -0,0 +1,7 @@
+package com.dinfactory.dincode.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DincodeController {
+}
---
+++ src/main/java/com/dinfactory/dincode/DincodeApplication.java
@@ -0,0 +1,14 @@
+package com.dinfactory.dincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DincodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DincodeApplication.class, args);
+	}
+
+
+}
